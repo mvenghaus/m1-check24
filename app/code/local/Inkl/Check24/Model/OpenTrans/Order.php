55a969797242d02cbbc683483f71dfb9ee0992e6
Marcus <info@inklammern.de>
2017-05-16 20:49:00 +0200

    quote product is now real product

--- app/code/local/Inkl/Check24/Model/OpenTrans/Order.php
+++ app/code/local/Inkl/Check24/Model/OpenTrans/Order.php
@@ -140,13 +140,7 @@ class Inkl_Check24_Model_OpenTrans_Order
 		foreach ($orderItemElements as $orderItemElement)
 		{
 			$sku = $this->xpathQuery('//SUPPLIER_PID', '', $orderItemElement);
-			$baseProduct = Mage::getModel('catalog/product')->loadByAttribute('sku', $sku);
-
-			$product = Mage::getModel('catalog/product')
-                ->setId($baseProduct->getId())
-				->setTypeId('simple')
-				->setSku($sku)
-				->setTaxClassId($baseProduct->getTaxClassId())
+			$product = Mage::getModel('catalog/product')->loadByAttribute('sku', $sku)
 				->setName($this->xpathQuery('//REMARK[@type=\'product_name\']', '', $orderItemElement))
 				->setPrice($this->xpathQuery('//PRICE_AMOUNT', '', $orderItemElement))
 				->setFinalPrice($this->xpathQuery('//PRICE_AMOUNT', '', $orderItemElement));
